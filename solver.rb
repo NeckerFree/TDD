baa20571198318dfd3b358661492d820d2972bf2
amrendrakind <vision2010@gmail.com>
2022-07-01 22:11:59 +0530

    Update: Find Factorial of given number

--- solver.rb
+++ solver.rb
@@ -1,8 +1,12 @@
 class Solver
 
-    def factorial(n)
-
-    end
+    def factorial(n) 
+        if n==0 
+            return 1 
+        else 
+            return n * factorial(n-1) 
+        end 
+    end 
     
     def reverse(word) 
 
