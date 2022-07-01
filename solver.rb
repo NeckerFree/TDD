dc030c2f9f40561d9c8ccedb0b44de8b23e13196
amrendrakind <vision2010@gmail.com>
2022-07-01 22:56:21 +0530

    Linter error fixed

--- solver.rb
+++ solver.rb
@@ -1,28 +1,27 @@
 class Solver
-
-    def factorial(n) 
-        if n==0 
-            return 1 
-        else 
-            return n * factorial(n-1) 
-        end 
-    end 
-    
-    def reverse(word) 
-        return word.reverse
+  def factorial(num)
+    if num.zero?
+      1
+    else
+      num * factorial(num - 1)
     end
+  end
 
-    def fizzbuzz(num)
-            fizz = (num % 3 == 0)
-            buzz = (num % 5 == 0)
-                  if fizz && buzz
-                       return "fizzbuzz"
-                  elsif fizz
-                       return "fizz"
-                  elsif buzz
-                       return "buzz"
-                  else
-                      return num.to_s
-                 end
-             end
-end    
+  def reverse(word)
+    word.reverse
+  end
+
+  def fizzbuzz(num)
+    fizz = (num % 3).zero?
+    buzz = (num % 5).zero?
+    if fizz && buzz
+      'fizzbuzz'
+    elsif fizz
+      'fizz'
+    elsif buzz
+      'buzz'
+    else
+      num.to_s
+    end
+  end
+end
